148b44e0cf167be6428d1f32c9bccd4a73b6a99e
Franspai <franspaismail@gmail.com>
2021-10-13 16:53:55 +0200

    ZombieKiller.java

---
+++ ZombieKiller.java
@@ -0,0 +1,25 @@
+
+public class ZombieKiller {
+
+	public static void main(String[] args) {
+		System.out.println("Im here to kick bubblegum and chew ass, and I'm all out of ass");
+
+		
+		
+		
+		String backpack [] = { "Shotgun", "Assult Riffle", "Sniper"};
+		
+		String zombies [] = { "Close-range zombie", "Mid-range zombie", "Long-range zombie"};
+		
+		System.out.println("Backpack items");
+		System.out.println(backpack[0]);
+		System.out.println(backpack[1]);
+		System.out.println(backpack[2]);
+		
+		System.out.println("Enemies");
+		System.out.println(zombies[0]);
+		System.out.println(zombies[1]);
+		System.out.println(zombies[2]);
+	}
+
+}
